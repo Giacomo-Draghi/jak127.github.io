13eef88a9f669d1c49e2a5428897537e07549f34
jak127 <jak127@byui.edu>
2019-10-08 20:40:38 +0200

    mid work

---
+++ lesson4/static/js/myscript.js
@@ -0,0 +1,9 @@
+//Function to get current year
+document.getElementById('Year').innerHTML = new Date().getFullYear();
+
+//Get document last modified date
+document.getElementById('lastUpdated').innerHTML = "Last Updated: " + document.lastModified;
+
+function toggleMenu() {
+	document.getElementsByClassName("menu-ul")[0].classList.toggle("responsive");
+}
