bbf8e66e35754e23de7e9d25108a9341de9ec3b3
jak127 <jak127@byui.edu>
2019-10-31 22:44:54 +0100

    update

--- lesson7/static/js/lazyload.js
+++ lesson7/static/js/lazyload.js
@@ -1,4 +1,4 @@
-const images = document.querySelectorAll('img[data-src]');
+/*const images = document.querySelectorAll('img[data-src]');
 
 function preloadImage(img) {
     const src = img.getAttribute('data-src');
@@ -9,10 +9,8 @@ function preloadImage(img) {
     img.src = src;
 }
 
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 300px 0px"
-};
+
+
 
 const imgObserver = new IntersectionObserver ((entries, imgObserver) => {
     entries.forEach(entry => {
@@ -27,4 +25,20 @@ const imgObserver = new IntersectionObserver ((entries, imgObserver) => {
  
 images.forEach(image => {
     imgObserver.observe(image);
-})
+}) */
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -300px 0px"
+};
+
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
