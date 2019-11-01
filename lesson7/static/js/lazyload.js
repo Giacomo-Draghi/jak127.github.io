1127661714d0cea5e456ab7e889bf34888ef58b3
jak127 <jak127@byui.edu>
2019-11-01 11:57:36 +0100

    update

--- lesson7/static/js/lazyload.js
+++ lesson7/static/js/lazyload.js
@@ -9,7 +9,7 @@ const loadImages = (image) => {
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px -300px 0px"
+    rootMargin: "0px 0px 300px 0px"
 };
 
 const imgObserver = new IntersectionObserver ((entries, imgObserver) => {
