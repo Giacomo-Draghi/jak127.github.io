19ee1462bb7b002397277257f4235e6589281cc2
jak127 <jak127@byui.edu>
2019-11-21 15:59:36 +0100

    up

--- lesson10/static/js/preston_api.js
+++ lesson10/static/js/preston_api.js
@@ -14,7 +14,7 @@ fetch(apiURL)
     document.querySelector('span#humidity').append(humidity);
     let speed = data.wind.speed;
     document.querySelector('span#speed').append(speed);
-    var chill, chillValue;
+    /*var chill, chillValue;
     //temperature = parseFloat(document.getElementById('temperature').innerHTML);
     //speed = parseFloat(document.getElementById('speed').innerHTML);
     chill = document.getElementById('chill');
@@ -24,16 +24,16 @@ fetch(apiURL)
       chill.innerHTML = chillValue;
     } else {
       chill.innerHTML = 'N/A';
-    }
+    }*/
 });
 
-function compute_wind_chill(temperature, speed) {
+/*function compute_wind_chill(temperature, speed) {
     const T = temperature;
     const W = speed;
     const WC = 35.74 + (0.6215 * T) - (35.75 * speed ** 0.16) + (0.4275 * T * W ** 0.16)
 
     return Math.floor(WC)
-}
+}*/
 
 fetch(apiURL2)
   .then((response) => response.json())
