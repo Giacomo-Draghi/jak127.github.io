3928ac7dbe123d1991ebded8adf51181c0092c2b
jak127 <jak127@byui.edu>
2019-11-21 15:58:47 +0100

    up

--- lesson10/static/js/preston_api.js
+++ lesson10/static/js/preston_api.js
@@ -15,19 +15,19 @@ fetch(apiURL)
     let speed = data.wind.speed;
     document.querySelector('span#speed').append(speed);
     var chill, chillValue;
-  //temperature = parseFloat(document.getElementById('temperature').innerHTML);
-  //speed = parseFloat(document.getElementById('speed').innerHTML);
-  chill = document.getElementById('chill');
-  chillValue = compute_wind_chill(temperature, speed);
+    //temperature = parseFloat(document.getElementById('temperature').innerHTML);
+    //speed = parseFloat(document.getElementById('speed').innerHTML);
+    chill = document.getElementById('chill');
+    chillValue = compute_wind_chill(temperature, speed);
 
-  if (temperature <= 50 && speed > 3) {
-    chill.innerHTML = chillValue;
-  } else {
-    chill.innerHTML = 'N/A';
-  }
-  });
+    if (temperature <= 50 && speed > 3) {
+      chill.innerHTML = chillValue;
+    } else {
+      chill.innerHTML = 'N/A';
+    }
+});
 
-  function compute_wind_chill(temperature, speed) {
+function compute_wind_chill(temperature, speed) {
     const T = temperature;
     const W = speed;
     const WC = 35.74 + (0.6215 * T) - (35.75 * speed ** 0.16) + (0.4275 * T * W ** 0.16)
