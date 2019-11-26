5d6177929bd05a422f44136c04c12857bf45a0c2
jak127 <jak127@byui.edu>
2019-11-26 16:06:28 +0100

    up

--- lesson11/static/js/preston_api.js
+++ lesson11/static/js/preston_api.js
@@ -43,9 +43,11 @@ fetch(apiURL)
     return Math.floor(WC)
 }*/
 
-/*fetch(apiURL2)
-  .then((response) => response.json())
-  .then((jsObject) => {
+const apiURL2 ='https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=2b91a52d5a023d1d2b5394b4e8268e24';
+
+fetch(apiURL2)
+  .then(response => response.json())
+  .then(jsObject => {
     console.log(jsObject);
     let forecast_list = jsObject.list;
     let count = 1;
@@ -60,13 +62,13 @@ fetch(apiURL)
         let imagealt = '//openweathermap.org/img/w/' + forecast_list[i].weather[0].description;
 
 
-        document.getElementById("day " + count).textContent = day;
-        document.getElementById("img " + count).setAttribute('src', imagesrc);
-        document.getElementById("img " + count).setAttribute('alt', imagealt);
-        document.getElementById("tem " + count).textContent = forecast_list[i].main.temp + "°F";
+        document.getElementById("day" + count).textContent = day;
+        document.getElementById("img" + count).setAttribute('src', imagesrc);
+        document.getElementById("img" + count).setAttribute('alt', imagealt);
+        document.getElementById("tem" + count).textContent = forecast_list[i].main.temp + "°F";
         
               
         count += 1;  
       }
     }
-  }); */
+  });
