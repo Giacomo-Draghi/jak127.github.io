62fbdb47b4802081024309946a41c2581c8ec079
jak127 <jak127@byui.edu>
2019-11-26 16:50:32 +0100

    up

--- lesson11/static/js/event.js
+++ lesson11/static/js/event.js
@@ -3,26 +3,37 @@ fetch(requestURL) .then(function (response) {
     return response.json();
 })
 .then(function (jsonObject) {
-    const prophets = jsonObject['towns'];
-    for (let i = 0; i < prophets.length; i++ ) {
+    const towns = jsonObject['towns'];
+    const eve = towns[4].events;
+    let h3 = document.createElement('h3');
+    h3.textContent = 'Upcoming Events in Preston: ';
+    document.querySelector('div.events').appendChild(h3);
+    for (let i = 0; i < eve.length; i++ ) {
         let event = document.createElement('section');
-        let h2 = document.createElement('h2');
         let p1 = document.createElement('p');
-        let p2 = document.createElement('p');
-        let image = document.createElement('img');
+        const txt = 'Upcoming Events in Preston: ';
 
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        p1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-        p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-        image.setAttribute('src', prophets[i].imageurl);
-        image.setAttribute('alt',prophets[i].name +' '+ prophets[i].lastname + '-'+ prophets[i].order);
+        p1.textContent = towns[4].events[i] + 'br' ;
 
-        card.appendChild(h2);
-        card.appendChild(p1);
-        card.appendChild(p2);
-        card.appendChild(image);
+        event.appendChild(p1);
 
-        document.querySelector('div.cards').appendChild(card);
-    }  
-    
+        document.querySelector('div.events').appendChild(event);
+    } 
 });
+
+let date_today = new Date();
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dececember"];
+let days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let is_friday = date_today.getDay();
+
+window.onload = function () {
+    document.getElementById('Year').innerHTML = date_today.getFullYear();
+    document.getElementById('date').innerHTML = days_of_the_week[date_today.getDay()] +', '+ date_today.getDate() + ' ' + months[date_today.getMonth()] + ' ' + date_today.getFullYear()
+
+    let friday_banner = document.getElementById('friday-banner');
+
+    if (is_friday == 5) {
+        friday_banner.style.display = "block";
+        on_resize()
+    }
+}
--- lesson11/static/js/myscript.js
+++ lesson11/static/js/myscript.js
@@ -5,23 +5,6 @@ function toggleMenu() {
 	document.getElementsByClassName("menu-ul")[0].classList.toggle("responsive");
 }
 
-let date_today = new Date();
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Dececember"];
-let days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let is_friday = date_today.getDay();
-
-window.onload = function () {
-    document.getElementById('Year').innerHTML = date_today.getFullYear();
-    document.getElementById('date').innerHTML = days_of_the_week[date_today.getDay()] +', '+ date_today.getDate() + ' ' + months[date_today.getMonth()] + ' ' + date_today.getFullYear()
-
-    let friday_banner = document.getElementById('friday-banner');
-
-    if (is_friday == 5) {
-        friday_banner.style.display = "block";
-        on_resize()
-    }
-}
-
 function on_resize() {
     let header_element = document.getElementById('headerElement');
     let int_viewport_width = window.innerWidth;
