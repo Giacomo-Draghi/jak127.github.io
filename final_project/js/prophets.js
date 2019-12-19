eb24896a353953f7623aaf223e100aca7fe78fd1
jak127 <jak127@byui.edu>
2019-12-19 15:42:58 +0100

    up

--- final_project/js/prophets.js
+++ final_project/js/prophets.js
@@ -1,21 +1,30 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://jak127.github.io/final_project/temple.json';
 fetch(requestURL) .then(function (response) {
     return response.json();
 })
 .then(function (jsonObject) {
-    const prophets = jsonObject['prophets'];
-    for (let i = 0; i < prophets.length; i++ ) {
-        let card = document.createElement('section');
+    const temple = jsonObject['temple'];
+    for (let i = 0; i < temple.length; i++ ) {
+        
+
+        
+    }  
+    
+});
+
+
+/*
+let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
         let image = document.createElement('img');
 
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        p1.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-        p2.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-        image.setAttribute('src', prophets[i].imageurl);
-        image.setAttribute('alt',prophets[i].name +' '+ prophets[i].lastname + '-'+ prophets[i].order);
+        h2.textContent = temple[i].name + ' ' + temple[i].lastname;
+        p1.textContent = 'Date of Birth: ' + temple[i].birthdate;
+        p2.textContent = 'Place of Birth: ' + temple[i].birthplace;
+        image.setAttribute('src', temple[i].imageurl);
+        image.setAttribute('alt',temple[i].name +' '+ temple[i].lastname + '-'+ temple[i].order);
 
         card.appendChild(h2);
         card.appendChild(p1);
@@ -23,6 +32,4 @@ fetch(requestURL) .then(function (response) {
         card.appendChild(image);
 
         document.querySelector('div.cards').appendChild(card);
-    }  
-    
-});
+        */
