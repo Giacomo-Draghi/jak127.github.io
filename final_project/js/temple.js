3a5776091f9646e1d802583782c78afba4f55195
jak127 <jak127@byui.edu>
2019-12-19 16:20:51 +0100

    up

--- final_project/js/temple.js
+++ final_project/js/temple.js
@@ -6,10 +6,30 @@ fetch(requestURL) .then(function (response) {
     const temple = jsonObject['temple'];
     for (let i = 0; i < temple.length; i++ ) {
         
-        let json = document.createElement ('section');
-        
+        let div = document.createElement ('div');
+        div.setAttribute('class', 'box-' + [i]);
+        let sec = document.createElement ('section');
+        let div2 = document.createElement ('div');
+        let h2 = document.createElement('h2');
+        let p1 = document.createElement('p');
+        let br = document.createElement('br');
+        let img = document.createElement('img');
+        //img.setAttribute('src', 'img/image-placeholder1.jpg');
+        img.setAttribute ('src', temple[i].imageurl);
+        img.setAttribute ('alt', temple[i].name);
 
+        h2.textContent = temple[i].name;
+        p1.textContent = 'Address: ' + temple[i].address + '<br>' +'Telephone: '+ temple[i].telephone + br + 'E-mail: ' + temple[i].email + br + 'Services: ' + temple[i].services + br +
+            'Dedicated on: ' + temple[i].hystory + br + 'Ordinance Schedule: ' + br + temple[i].session1 + br + temple[i].session2 + br + 'Closure: ' + br + temple[i].closure;
         
+        sec.appendChild(h2);
+        sec.appendChild(p1);
+        div2.appendChild(img);
+        div.appendChild(div2);
+        div.appendChild(sec);
+
+        document.querySelector('div.json').appendChild(div);
+
     }  
     
 });
