de61cc6303aa955bc4d6d2a0f9521d15cbe7faf0
jak127 <jak127@byui.edu>
2019-12-19 22:44:18 +0100

    up

--- final_project/js/temple.js
+++ final_project/js/temple.js
@@ -21,6 +21,11 @@ fetch(requestURL) .then(function (response) {
         let p7 = document.createElement('p');
         let p8 = document.createElement('p');
         let hh4 = document.createElement('h4');
+        let hhh4 = document.createElement('h4');
+        let p9 = document.createElement('p');
+        p9.setAttribute('class', 'description-' + [i]);
+        let p10 = document.createElement('p');
+        p10.setAttribute('class', 'temp-' + [i]);
         let img = document.createElement('img');
         img.setAttribute ('src', temple[i].imageurl);
         img.setAttribute ('alt', temple[i].name);
@@ -36,6 +41,9 @@ fetch(requestURL) .then(function (response) {
         p7.textContent = temple[i].session2;
         hh4.textContent ='Closure:';
         p8.textContent = temple[i].closure;
+        hhh4.textContent = 'Weather summary';
+        p9.textContent = 'Currently: ';
+        p10.textContent = 'Temperature: ';
         
         sec.appendChild(h2);
         sec.appendChild(p2);
@@ -47,36 +55,42 @@ fetch(requestURL) .then(function (response) {
         sec.appendChild(p7);
         sec.appendChild(hh4);
         sec.appendChild(p8);
+        sec.appendChild(hhh4);
+        sec.appendChild(p9);
+        sec.appendChild(p10);
         div2.appendChild(img);
         div.appendChild(div2);
         div.appendChild(sec);
 
         document.querySelector('div.json').appendChild(div);
 
-    }  
+        var apiURL ='https://api.openweathermap.org/data/2.5/weather?id=4976934&units=imperial&APPID=2b91a52d5a023d1d2b5394b4e8268e24';
+
+        if (i == 0){
+            var apiURL ='https://api.openweathermap.org/data/2.5/weather?id=4976934&units=imperial&APPID=2b91a52d5a023d1d2b5394b4e8268e24';
+        } else if (i == 1) {
+            var apiURL ='https://api.openweathermap.org/data/2.5/weather?id=2988507&units=imperial&APPID=2b91a52d5a023d1d2b5394b4e8268e24';
+        } else if (i == 2) {
+            var apiURL ='https://api.openweathermap.org/data/2.5/weather?id=3117735&units=imperial&APPID=2b91a52d5a023d1d2b5394b4e8268e24';
+        } else if (i == 3){
+            var apiURL ='https://api.openweathermap.org/data/2.5/weather?id=2643743&units=imperial&APPID=2b91a52d5a023d1d2b5394b4e8268e24';
+        }
+        
+
+        let des = 'p.description-' + [i];
+        let tem = 'p.temp-' + [i];
+        fetch(apiURL)
+          .then((response) => response.json())
+          .then((jsObject) => {
+            console.log(jsObject);
+            let data = jsObject;
+            let current = data.weather[0].main;
+            document.querySelector(des).append(current);
+            let temperature = data.main.temp;
+            document.querySelector(tem).append(temperature);
+            });
+        }  
     
 });
 
 
-
-
-/*
-let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let p1 = document.createElement('p');
-        let p2 = document.createElement('p');
-        let image = document.createElement('img');
-
-        h2.textContent = temple[i].name + ' ' + temple[i].lastname;
-        p1.textContent = 'Date of Birth: ' + temple[i].birthdate;
-        p2.textContent = 'Place of Birth: ' + temple[i].birthplace;
-        image.setAttribute('src', temple[i].imageurl);
-        image.setAttribute('alt',temple[i].name +' '+ temple[i].lastname + '-'+ temple[i].order);
-
-        card.appendChild(h2);
-        card.appendChild(p1);
-        card.appendChild(p2);
-        card.appendChild(image);
-
-        document.querySelector('div.cards').appendChild(card);
-        */
